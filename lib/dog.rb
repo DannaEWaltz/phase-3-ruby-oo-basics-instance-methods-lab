237d8dba60b1c016b4ec35614f0d925c7bacf6a4
+ <+dannaewaltz@gmail.com>
2022-08-14 09:44:30 -0600

    commit tree 2440de1d972f90b30c897b16e67dc7257e84dbed

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+    def bark
+        puts "Woof!"
+    end
+    def sit 
+        puts "The Dog is sitting"
+    end
+end
+
+Fido = Dog.new
+Fido.bark
+Snoopy = Dog.new
+Snoopy.bark
+Fido.sit
+Snoopy.sit
+
+
+
+
+
